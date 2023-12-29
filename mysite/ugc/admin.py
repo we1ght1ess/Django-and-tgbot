b9c1cf3ba045737ab88533764940d5b934feefc3
Nikita Korostelev <144522028+we1ght1ess@users.noreply.github.com>
2023-12-30 03:20:48 +0700

    Update admin.py

--- mysite/ugc/admin.py
+++ mysite/ugc/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Profile
 from .models import Message
 from .forms import ProfileForm
+import telebot
+from django.conf import settings
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
@@ -11,5 +13,17 @@ class ProfileAdmin(admin.ModelAdmin):
 @admin.register(Message)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'profile', 'text', 'created_at', 'send')
+    list_filter = ('send',)
 
-    
+    def show_message(self, request, queryset):
+        bot_token = settings.TOKEN  
+        bot = telebot.TeleBot(bot_token)
+        message_text = "Здравствуйте! Это рассылка"
+        for message in queryset:
+            chat_id = message.profile.external_id
+            bot.send_message(chat_id, message_text)
+
+        self.message_user(request, "Рассылка запущена")
+    show_message.short_description = "Создать рассылку"
+
+    actions = [show_message]
